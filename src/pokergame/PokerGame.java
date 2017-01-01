504be3f742adddee670f8c57cf67d08bbb852ab2
sandoval747 <mdevries19@gmail.com>
2017-01-01 15:49:50 -0500

    Version 1 - 1Jan2017

---
+++ src/pokergame/PokerGame.java
@@ -0,0 +1,16 @@
+package pokergame;
+
+/**
+ *
+ * @author Maurice
+ */
+public class PokerGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
